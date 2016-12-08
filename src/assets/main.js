55420bf65b952535a3935cd1365871e30541b80c
JB Wood <wood.jdesign@gmail.com>
2016-12-07 21:13:49 -0500

    Completed firs set of tasks

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,18 @@
 $(function() {
 
   // your code will go here
-
+  $.ajax({
+    url:'https://www.codeschool.com/users/Ginja_Beard.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      console.log(response.courses.completed.length);
+      var completed = response.courses.completed;
+      for(var i = 0; i < completed.length; i++ ){
+        var badgeDiv = $('<div></div>').addClass('course').appendTo('#badges');
+        $('<h3>'+completed[i].title+'</h3>').appendTo(badgeDiv);
+        $('<img src="'+completed[i].badge+'" />').appendTo(badgeDiv);
+        $('<a href="'+completed[i].url+'">Go To Course</a>').addClass('btn btn-primary').appendTo(badgeDiv);
+      }
+    }
+  });
 });
